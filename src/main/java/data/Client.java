7725186550a9c6c0a87bde78e1114307da73372f
IvanStek <ivan-stek@mail.ru>
2024-04-05 16:18:35 +0300

    Made a Card and Client realisation

--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,40 @@
 package data;
 
+import bank.Card;
+
 public class Client {
+    private String name;
+    private String surName;
+    private Card card;
+    private String isClient;
+
+    public Client(String name, String surName){
+        this.name = name;
+        this.surName = surName;
+        card = new Card();
+    }
+
+    public Card getCard() {
+        return card;
+    }
+
+    public void setCard(Card card) {
+        this.card = card;
+    }
+
+    public String getIsClient() {
+        return isClient;
+    }
+
+    public void setIsClient(String isClient) {
+        this.isClient = isClient;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
 }
